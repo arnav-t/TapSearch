9b9dc1c6a71517bf85432101bc225ca353e8a23e
arnav-t <avznav@gmail.com>
2019-11-23 13:53:16 +0530

    Add functionality to save and index paragraph

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+from gen_indices import gen_indices
+
+API_ENDPOINT = 'api'
+PARAGRAPH_DELIMITER = '\n\n'
+WORD_DELIMITER = ' '
+
+app = Flask(__name__)		# Start flask app
+
+paragraphs = []		# Paragraphs list
+reverseIndices = {}		# Reverse indices for the paragraphs
+indexed = set()		# Set of indexed paragraphs 
+
+@app.route(f'/{API_ENDPOINT}/get')
+def get_paragraphs():
+	"""Return all paragraphs."""
+	return jsonify(paragraphs)
+
+@app.route(f'/{API_ENDPOINT}/add', methods=['POST'])
+def add_paragraphs():
+	"""Add paragraphs to memory and return all paragraphs."""
+	newParagraphs = request.get_json(force=True)['data']
+	# Split by delimiter and add to list
+	paragraphs.extend(newParagraphs.split(PARAGRAPH_DELIMITER))
+	return jsonify(paragraphs)
+
+@app.route(f'/{API_ENDPOINT}/index')
+def index_paragraph():
+	"""Index a given paragraph from given id otherwise index all and return generated indices."""
+	id = request.args.get('id')
+	if id is None:
+		# Index all paragraphs
+		for id, paragraph in enumerate(paragraphs):
+			# Verify if not already indexed
+			if id not in indexed:
+				gen_indices(reverseIndices, id, paragraph, WORD_DELIMITER)
+				indexed.add(id)
+	else:
+		try:
+			# Convert id to integer
+			id = int(id)
+		except ValueError:
+			# Invalid id given
+			return 'Invalid id', 400
+		# Check for bounds
+		if id < 0 or id >= len(paragraphs):
+			return 'Not Found', 404
+		# Verify if not already indexed
+		if id not in indexed:
+			gen_indices(reverseIndices, id, paragraphs[id], WORD_DELIMITER)
+			indexed.add(id)
+	return jsonify(reverseIndices)
---
+++ gen_indices.py
@@ -0,0 +1,21 @@
+def gen_indices(indices, id, paragraph, delimiter):
+	"""Split words and store them in an existing reverse index"""
+	# Replace newline characters
+	paragraph = paragraph.replace('\n', ' ')
+	# Split words by delimiter
+	words = [word.strip() for word in paragraph.split(delimiter)]
+	# Save number of occurences of word in a reverse index
+	for word in words:
+		if word:
+			if word not in indices.keys():
+				# Create new key if not present already
+				indices[word] = {}
+				indices[word][id] = 1
+			else:
+				# Increment if already present
+				# Create paragraph id if not already present
+				if id not in indices[word].keys():
+					indices[word][id] = 1
+				else:
+					# Else just increment it
+					indices[word][id] += 1
